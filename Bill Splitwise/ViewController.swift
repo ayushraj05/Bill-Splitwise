fed3ea7c887e710cda971ea3a3bc2502cba1615c
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-05-22 00:47:52 +0530

    Initial Commit

---
+++ Bill Splitwise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill Splitwise
+//
+//  Created by Ayush Rajpal on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
